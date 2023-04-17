1a36d0fed4d2c63c13f3075907f3d3ca79a05f6b
olegweremey <olegweremey1994@mail.ru>
2023-04-17 21:37:29 +0300

    feature: active nav item

--- src/components/TheNav/TheNav.vue
+++ src/components/TheNav/TheNav.vue
@@ -1,12 +1,20 @@
 <script setup>
 import {ChartBarIcon, ClockIcon, ListBulletIcon} from "@heroicons/vue/24/outline";
 import NavItem from "@/components/TheNav/NavItem/NavItem.vue";
+import {ref} from "vue";
 
 const navItems = {
   timeline: ClockIcon,
   activities: ListBulletIcon,
   progress: ChartBarIcon
 }
+
+let currenPage = ref("timeline")
+
+const setCurrentPage = (value) => {
+  currenPage.value = value
+}
+
 </script>
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
@@ -15,6 +23,8 @@ const navItems = {
           v-for="(icon, page) in navItems"
           :key="page"
           :href="`#${page}`"
+          :class="{'bg-gray-200 pointer-events-none' : currenPage === page}"
+          @click="setCurrentPage(page)"
       >
         <component :is="icon" class="h-6 w-6"/>
         {{ page }}
