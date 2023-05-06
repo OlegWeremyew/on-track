79d09c8bfb165f341e976b049d8ac503032c6e2f
olegweremey <olegweremey1994@mail.ru>
2023-05-06 23:25:15 +0300

    added: ActivitiesItem component

--- src/pages/TheActivities/TheActivities.vue
+++ src/pages/TheActivities/TheActivities.vue
@@ -1,22 +1,5 @@
 <script setup>
-import { TrashIcon } from '@heroicons/vue/24/outline'
-import BaseButton from '@/components/common/BaseButton/BaseButton.vue'
-import BaseSelect from '@/components/common/BaseSelect/BaseSelect.vue'
-
-const periodSelectOptions = [
-  {
-    value: 15,
-    label: '0:15',
-  },
-  {
-    value: 30,
-    label: '0:30',
-  },
-  {
-    value: 45,
-    label: '0:45',
-  },
-]
+import ActivityItem from '@/components/ActivityItem/ActivityItem.vue'
 
 const activities = ['Coding', 'Reading', 'Training']
 </script>
@@ -24,17 +7,7 @@ const activities = ['Coding', 'Reading', 'Training']
 <template>
   <div>
     <ul class='divide-y'>
-      <li v-for='activity in activities' :key='activity' class='flex flex-col p-4 gap-2'>
-        <div class='flex items-center gap-2'>
-          <BaseButton>
-            <TrashIcon class='h-8' />
-          </BaseButton>
-          <span class='truncate text-xl'>{{ activity }}</span>
-        </div>
-        <div>
-          <BaseSelect class='font-mono' placeholder='h:mm' :options='periodSelectOptions' />
-        </div>
-      </li>
+      <ActivityItem  v-for='activity in activities' :key='activity' :activity='activity'/>
     </ul>
   </div>
 </template>
