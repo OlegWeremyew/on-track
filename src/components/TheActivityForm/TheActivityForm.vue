eb24030a0bd04d6c489c29b67556fc90a1c2b79c
olegweremey <olegweremey1994@mail.ru>
2023-05-24 21:51:33 +0300

    update: activity page render and scrolling

--- src/components/TheActivityForm/TheActivityForm.vue
+++ src/components/TheActivityForm/TheActivityForm.vue
@@ -2,33 +2,41 @@
 import BaseButton from '@/components/common/BaseButton/BaseButton.vue'
 import { PlusIcon } from '@heroicons/vue/24/outline'
 import { isActivityValid } from '@/validators'
-import { ref } from 'vue'
+import { nextTick, ref } from 'vue'
 
 const emit = defineEmits({
   submit: isActivityValid,
 })
 
-const activity = ref('');
+const activity = ref('')
 
-const submit = () => {
-  emit('submit', activity.value);
-  activity.value = '';
+const submit = async () => {
+  emit('submit', activity.value)
+
+  activity.value = ''
+
+  await nextTick()
+  window.scrollTo({
+    left: 0,
+    top: document.body.scrollHeight,
+    behavior: 'smooth',
+  })
 }
 </script>
 
 <template>
   <form
     class='sticky bottom-[57px] flex gap-2 border-t bg-white p-4'
-    @submit.prevent="submit"
+    @submit.prevent='submit'
   >
     <input
       type='text'
       class='w-full rounded border px-4 text-xl'
       placeholder='Activity name'
-      v-model="activity"
+      v-model='activity'
     >
     <BaseButton :disabled='!activity.trim()'>
       <PlusIcon class='h-8' />
-    </BaseButton >
+    </BaseButton>
   </form>
 </template>
--- src/pages/TheActivities/TheActivities.vue
+++ src/pages/TheActivities/TheActivities.vue
@@ -2,6 +2,7 @@
 import ActivityItem from '@/components/ActivityItem/ActivityItem.vue'
 import { isActivityValid, validateActivities } from '@/validators'
 import TheActivityForm from '@/components/TheActivityForm/TheActivityForm.vue'
+import TheActivitiesEmptyState from '@/components/TheActivitiesEmptyState/TheActivitiesEmptyState.vue'
 
 defineProps({
   activities: {
@@ -19,8 +20,8 @@ const emit = defineEmits({
 </script>
 
 <template>
-  <div>
-    <ul class='divide-y'>
+  <div class='flex flex-col grow'>
+    <ul v-if='activities.length' class='divide-y grow'>
       <ActivityItem
         v-for='activity in activities'
         :key='activity'
@@ -28,6 +29,7 @@ const emit = defineEmits({
         @delete="emit('deleteActivity', activity)"
       />
     </ul>
+    <TheActivitiesEmptyState v-else />
     <TheActivityForm @submit="emit('createActivity', $event)" />
   </div>
 </template>
