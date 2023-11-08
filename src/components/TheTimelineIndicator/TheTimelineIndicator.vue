68b8742a99c87f9efd95be227c7dea1b83e20341
olegweremey <olegweremey1994@mail.ru>
2023-11-08 21:47:52 +0300

    feat: added indicator handler

--- src/components/TheTimelineIndicator/TheTimelineIndicator.vue
+++ src/components/TheTimelineIndicator/TheTimelineIndicator.vue
@@ -1,26 +1,15 @@
 <script setup>
-import { ref } from 'vue'
-import { HUNDRED_PERCENT, MINUTES_IN_HOUR, SECONDS_IN_DAY, SECONDS_IN_MINUTE } from '@/constants'
+import { computed, ref } from 'vue'
+import { HUNDRED_PERCENT } from '@/constants'
+import { useSecondsSinceMidnight } from '@/composables/seconds-since-midnight'
 
 const indicatorRef = ref()
 
-function calculateTopOffset() {
-  return (calculateSecondsSinceMidnightInPercentage() * getTimelineHeight()) / HUNDRED_PERCENT
-}
+const {secondsSinceMidnightInPercentage} = useSecondsSinceMidnight()
 
-function calculateSecondsSinceMidnightInPercentage() {
-  return (HUNDRED_PERCENT * calculateSecondsSinceMidnight()) / SECONDS_IN_DAY
-}
-
-function calculateSecondsSinceMidnight() {
-  const now = new Date()
-
-  return (
-    SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
-    SECONDS_IN_MINUTE * now.getMinutes() +
-    now.getSeconds()
-  )
-}
+const topOffset = computed(() => {
+  return (secondsSinceMidnightInPercentage.value * getTimelineHeight()) / HUNDRED_PERCENT
+})
 
 function getTimelineHeight() {
   return indicatorRef.value?.parentNode.getBoundingClientRect().height
@@ -31,6 +20,6 @@ function getTimelineHeight() {
   <hr
     ref='indicatorRef'
     class='pointer-events-none absolute z-10 w-full border-b-2 border-red-600'
-    :style='{ top: `${calculateTopOffset()}px` }'
+    :style='{ top: `${topOffset}px` }'
   />
 </template>
---
+++ src/composables/seconds-since-midnight.js
@@ -0,0 +1,48 @@
+import { computed, onActivated, onDeactivated, ref, watchEffect } from 'vue'
+import {
+  HUNDRED_PERCENT,
+  MILLISECONDS_IN_SECONDS,
+  MINUTES_IN_HOUR,
+  SECONDS_IN_DAY,
+  SECONDS_IN_MINUTE,
+} from '@/constants'
+
+function calculateSecondsSinceMidnight() {
+  const now = new Date()
+
+  return (
+    SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
+    SECONDS_IN_MINUTE * now.getMinutes() +
+    now.getSeconds()
+  )
+}
+
+export const useSecondsSinceMidnight = () => {
+  const secondsSinceMidnight = ref(calculateSecondsSinceMidnight())
+
+  const secondsSinceMidnightInPercentage = computed((() => {
+    return (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
+  }))
+
+  watchEffect(() => {
+    if (secondsSinceMidnight.value > SECONDS_IN_DAY) {
+      secondsSinceMidnight.value = 0
+    }
+  })
+
+  let timer = null
+
+  // page is active
+  onActivated(() => {
+    timer = setInterval(() => secondsSinceMidnight.value += 5 * 60, MILLISECONDS_IN_SECONDS)
+
+    secondsSinceMidnight.value = calculateSecondsSinceMidnight()
+  })
+
+// page is inactive
+  onDeactivated(() => clearInterval(timer))
+
+  return {
+    secondsSinceMidnightInPercentage
+  }
+}
